3909467cecb8c1c660e4c30ab392dba9aa3fa761
daysongs <dayvsongoncalves121197@gmail.com>
2022-10-19 19:46:57 -0300

    exemplos das aulas orientação a objetos

---
+++ Aulas_Orientação_Objetos_Exemplo/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Carro carro1 = new Carro();
+
+        carro1.setCor("Azul");
+        carro1.setModelo("BMW série 3");
+        carro1.setCapacidadeTanque(59);
+
+        System.out.println(carro1.gatModelo());
+        System.out.println(carro1.gatCor());
+        System.out.println(carro1.gatCapacidadeTanque());
+        System.out.println(carro1.totalValorTanque(6.39));
+
+        Carro carro2 = new Carro("Ciza","Mercedez-Benz Classe C", 66);
+
+
+        System.out.println(carro2.gatModelo());
+        System.out.println(carro2.gatCor());
+        System.out.println(carro2.gatCapacidadeTanque());
+        System.out.println(carro2.totalValorTanque(6.46));
+        
+    }
+}
---
+++ Aulas_Orientação_Objetos_Exemplo/src/Carro.java
@@ -0,0 +1,46 @@
+ class Carro {
+    // Atributos
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+    // Costrutor
+    Carro(){
+
+    }
+
+    Carro(String cor, String modelo, int capacidadeTanque){
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+
+    // gat e set
+    void setCor(String cor){
+        this.cor = cor;
+    }
+
+    String gatCor(){
+        return cor;
+    }
+
+    void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    String gatModelo(){
+        return modelo;
+    }
+
+    void setCapacidadeTanque(int capacidadeTanque){
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    int gatCapacidadeTanque(){
+        return capacidadeTanque;
+    }
+    double totalValorTanque(double valorCombustivel){
+        return capacidadeTanque*valorCombustivel;
+    }
+}
